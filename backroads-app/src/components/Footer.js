908cb91c3df9d336c689ec4c07dca5be55a6e16f
f-ghalous <e2295817@cmaisonneuve.qc.ca>
2024-05-07 20:04:23 -0400

    mettre la date ds le footer

--- backroads-app/src/components/Footer.js
+++ backroads-app/src/components/Footer.js
@@ -4,36 +4,38 @@ const Footer = () => {
   return (
     <footer className="section footer">
       <ul className="footer-links">
-        {pageLinks.map((link)=>{
-          const {href, text, id} = link;
-          return(
-
-            <li key = {id}>
-             <a href={href} className="footer-link">
-               {text}
-             </a>
-           </li>
-          )
-
-        })}
-  é
-      </ul>
-      <ul className="footer-icons">
-        {socialLinks.map((link)=>{
-          const {href, icon, id} = link;
+        {pageLinks.map((link) => {
+          const { href, text, id } = link
           return (
             <li key={id}>
-              <a href={href} target="_blank" className="footer-icon">
+              <a href={href} className="footer-link">
+                {text}
+              </a>
+            </li>
+          )
+        })}
+        é
+      </ul>
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          const { href, icon, id } = link
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                rel="noopener"
+                className="footer-icon"
+              >
                 <i className={icon}></i>
               </a>
             </li>
           )
         })}
-       
       </ul>
       <p className="copyright">
         copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        <span id="date">{ new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
   )
